882399cb6cf436779e400adabedc3b5a491592c8
nikko <=>
2021-03-09 23:25:38 +0800

    Strict typing

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\EventRequest;
 use App\Http\Resources\EventResource;
 use App\Repositories\EventRepository;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class EventController extends Controller
 {
@@ -15,11 +16,11 @@ class EventController extends Controller
         $this->repository = $repository;
     }
 
-    public function getEvents()
+    public function getEvents(): ResourceCollection
     {
         return EventResource::collection($this->repository->getEvents());
     }
-    public function createEvent(EventRequest $request)
+    public function createEvent(EventRequest $request): EventResource
     {
         return (new EventResource($this->repository->createEvent($request->payload())));
     }
--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -4,15 +4,16 @@ namespace App\Repositories;
 
 use App\Models\Event;
 use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\QueryException;
 
 class EventRepository extends Event
 {
-	public function getEvents()
+	public function getEvents(): Collection
 	{
 		return $this->get();
 	}
-	public function createEvent($payload)
+	public function createEvent($payload): Event
 	{
 		return $this->create($payload);
 	}
